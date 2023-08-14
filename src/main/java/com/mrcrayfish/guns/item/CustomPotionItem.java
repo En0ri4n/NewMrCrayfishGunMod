4c023b143d4fe8d223d29702d6322124e103b562
Enorian <enorian.rajo@gmail.com>
2023-08-14 16:27:14 +0200

    Add potion description in potions tooltip

--- src/main/java/com/mrcrayfish/guns/item/CustomPotionItem.java
+++ src/main/java/com/mrcrayfish/guns/item/CustomPotionItem.java
@@ -1,14 +1,31 @@
 package com.mrcrayfish.guns.item;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CustomPotionItem extends net.minecraft.world.item.PotionItem
 {
     public CustomPotionItem()
     {
         super(new Item.Properties().stacksTo(1));
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag)
+    {
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
+
+        pTooltip.add(new TranslatableComponent("effect.cgm." + PotionUtils.getPotion(pStack).getName("") + ".desc").withStyle(ChatFormatting.GOLD, ChatFormatting.ITALIC));
     }
 
     @Override
